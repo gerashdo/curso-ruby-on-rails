105e3ac70675de3cc340d86524c3f08ae18c767c
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-11-07 20:02:20 -0600

    Add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def farenheit_to_celsius(farenheit)
+        "#{(farenheit - 32) * 5 / 9}°C"
+    end
+
+    # def hero_banner
+    #     styles = "display:flex;justify-content:center;min-height:500px;padding:1em;flex-direction:column;background-color:#eee;gap:1em;"
+    #     content_tag(:header, style: styles) do
+    #         yield
+    #     end
+    # end
+
+    def hero_banner(&block)
+        styles = "display:flex;justify-content:center;min-height:500px;padding:1em;flex-direction:column;background-color:#eee;gap:1em;"
+        content_tag(:header, style: styles, &block)
+    end
 end
